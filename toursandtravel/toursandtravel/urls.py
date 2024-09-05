c2d7acd09b0cdefd8a3aa7532ddd15e7ecbff736
suman154 <sumanbhatta735@gmail.com>
2024-09-05 17:15:51 +0545

    saf

--- toursandtravel/toursandtravel/urls.py
+++ toursandtravel/toursandtravel/urls.py
@@ -15,32 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Travel Nepal API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+from django.urls import path,include
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('api/v1/useraccounts/', include('useraccounts.urls')),
-    path('api/v1/packages/', include('packages.urls')),
-   path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('admin/', admin.site.urls),
+    # path('', include('packages.urls'))
 
 ]
