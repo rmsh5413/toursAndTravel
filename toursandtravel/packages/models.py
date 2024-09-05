e04dd6955f35821567f6a7cc3e81616b9718e128
rmsh5413 <71429461+rmsh5413@users.noreply.github.com>
2024-09-05 13:51:43 +0545

    added ordering and in meta tag

--- toursandtravel/packages/models.py
+++ toursandtravel/packages/models.py
@@ -8,6 +8,7 @@ from autoslug import AutoSlugField
 
 class HolidaysPackagesCategory(BaseModel):
     name = models.CharField(max_length=255)
+    ordering=models.PositiveBigIntegerField()
     slug = AutoSlugField(populate_from='name', unique=True)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='holidaypackagescategory', blank=True, null=True)
@@ -15,9 +16,14 @@ class HolidaysPackagesCategory(BaseModel):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        ordering = ['ordering']
+
 
 class HolidaysPackages(BaseModel):
     category = models.ForeignKey(HolidaysPackagesCategory, on_delete=models.CASCADE, related_name='packages')
+    ordering=models.PositiveBigIntegerField()
     name = models.CharField(max_length=255)
     slug = AutoSlugField(populate_from='name', unique=True)
     description = models.TextField(blank=True, null=True)
@@ -27,8 +33,13 @@ class HolidaysPackages(BaseModel):
     def __str__(self):
         return self.name
     
+    class Meta:
+        ordering = ['ordering']
+
+    
 class HolidaysPackagesItinerary(BaseModel):
     package = models.ForeignKey(HolidaysPackages, on_delete=models.CASCADE, related_name='itinerary')
+    ordering=models.PositiveBigIntegerField()
     day = models.PositiveIntegerField()
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
@@ -38,3 +49,5 @@ class HolidaysPackagesItinerary(BaseModel):
     def __str__(self):
         return self.title
     
+    class Meta:
+        ordering = ['ordering']
