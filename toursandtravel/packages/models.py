860ff0bcc3e3dfccdc7dcea76d1bbc8bbdbf196e
rmsh5413 <71429461+rmsh5413@users.noreply.github.com>
2024-09-05 13:34:34 +0545

    tourpackages models created

--- toursandtravel/packages/models.py
+++ toursandtravel/packages/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from basemodel.models import BaseModel
+from useraccounts.models import User
+from autoslug import AutoSlugField
 
 # Create your models here.
+
+
+class HolidaysPackagesCategory(BaseModel):
+    name = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from='name', unique=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='holidaypackagescategory', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class HolidaysPackages(BaseModel):
+    category = models.ForeignKey(HolidaysPackagesCategory, on_delete=models.CASCADE, related_name='packages')
+    name = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from='name', unique=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='holidaypackages', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+class HolidaysPackagesItinerary(BaseModel):
+    package = models.ForeignKey(HolidaysPackages, on_delete=models.CASCADE, related_name='itinerary')
+    day = models.PositiveIntegerField()
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='holidaypackagesitinerary', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
