5b3e88ccc8ce77c9e5c90cde08ff2b8b4212b3e0
suman154 <sumanbhatta735@gmail.com>
2024-09-05 17:19:03 +0545

    fsa

--- toursandtravel/toursandtravel/urls.py
+++ toursandtravel/toursandtravel/urls.py
@@ -1,6 +1,5 @@
 """
 URL configuration for toursandtravel project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
@@ -16,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('packages.urls'))
-
-
-
 ]
