97929efe4ba1f009b64770801d422ae262bacb0a
rmsh5413 <71429461+rmsh5413@users.noreply.github.com>
2024-09-05 16:27:24 +0545

    added fieds in packages

--- toursandtravel/packages/models.py
+++ toursandtravel/packages/models.py
@@ -5,15 +5,36 @@ from autoslug import AutoSlugField
 from ckeditor.fields import RichTextField
 # Create your models here.
 
+MEAL = (
+    ('Breakfast', 'Breakfast'),
+    ('Lunch', 'Lunch'),
+    ('Dinner', 'Dinner'),
+)
 
+SEASONS = (
+    ('Spring', 'Spring'),
+    ('Summer', 'Summer'),
+    ('Autumn', 'Autumn'),
+    ('Winter', 'Winter'),
+    ('All', 'All'),
+)
+
+ACCOMMODATION = (
+    ('Hotel', 'Hotel'),
+    ('Resort', 'Resort'),
+    ('Cottage', 'Cottage'),
+    ('Villa', 'Villa'),
+    ('Camp', 'Camp'),
+)
+    # existing code
 class HolidaysPackagesCategory(BaseModel):
     name = models.CharField(max_length=255)
     ordering=models.PositiveBigIntegerField()
     slug = AutoSlugField(populate_from='name', unique=True)
-    description = models.TextField(blank=True, null=True)
+    description = RichTextField()
+    equipment = RichTextField()
     image = models.ImageField(upload_to='holidaypackagescategory', blank=True, null=True)
     
-
     def __str__(self):
         return self.name
     
@@ -29,7 +50,17 @@ class HolidaysPackages(BaseModel):
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='holidaypackages', blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    destination = models.CharField(max_length=255)
+    duration = models.CharField(max_length=255)
+    start_point=models.CharField(max_length=255)
+    end_point=models.CharField(max_length=255)
+    group_size=models.CharField(max_length=255)
+    season=models.CharField(max_length=255, choices=SEASONS)
+    meals = models.CharField(max_length=255, choices=MEAL)
+    accommodations=models.CharField(max_length=255,choices=ACCOMMODATION)
+    activity_duration=models.CharField(max_length=255)
+    max_altitude=models.CharField(max_length=255)
+    youtubeUrl=models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.name
