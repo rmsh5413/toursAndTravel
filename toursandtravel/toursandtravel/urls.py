01fa7c002514204175b9574d9f1ba4b036f52b41
suman154 <sumanbhatta735@gmail.com>
2024-09-06 10:38:49 +0545

    App create for blog

--- toursandtravel/toursandtravel/urls.py
+++ toursandtravel/toursandtravel/urls.py
@@ -42,28 +42,14 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-
-
-
     path('api/v1/user/',include('useraccounts.urls')),
- 
-
-   
     path('api/v1/packages/',include('packages.urls')),
-   
-
     #auth
-  
     # path('accounts/', include('allauth.urls')),
     # path('auth/', include('dj_rest_auth.urls')),
     # path('auth/registration/', include('dj_rest_auth.registration.urls')),
-    
-
     #documentation
-    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-  
-
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
