f79a4a802229f35af3edf8a496e760d6db325c1d
rmsh5413 <71429461+rmsh5413@users.noreply.github.com>
2024-09-08 11:04:42 +0545

    updated in packages edit api

--- toursandtravel/packages/serializers.py
+++ toursandtravel/packages/serializers.py
@@ -110,7 +110,6 @@ class HolidaysPackagesSerializer(serializers.ModelSerializer):
 
 
 
-
 class UpdateHolidaysPackagesSerializer(serializers.ModelSerializer):
     faqs = HolidaysPackagesFaqSerializer(many=True, required=False)
     highlights = HolidaysPackagesHighlightsSerializer(many=True, required=False)
@@ -124,7 +123,7 @@ class UpdateHolidaysPackagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = HolidaysPackages
         fields = [
-            'id','country','city', 'packagetype', 'category', 'ordering', 'name', 'slug', 'description',
+            'id', 'country', 'city', 'packagetype', 'category', 'ordering', 'name', 'slug', 'description',
             'image', 'price', 'destination', 'duration', 'start_point', 'end_point',
             'group_size', 'season', 'meals', 'select_package_type', 'accommodations',
             'activity_duration', 'max_altitude', 'youtubeUrl', 'mapUrl', 'pdfManual',
@@ -132,7 +131,7 @@ class UpdateHolidaysPackagesSerializer(serializers.ModelSerializer):
         ]
 
     def update(self, instance, validated_data):
-        # Update the HolidaysPackages fields
+        # Update HolidaysPackages fields
         instance.name = validated_data.get('name', instance.name)
         instance.country = validated_data.get('country', instance.country)
         instance.city = validated_data.get('city', instance.city)
@@ -153,12 +152,10 @@ class UpdateHolidaysPackagesSerializer(serializers.ModelSerializer):
         instance.youtubeUrl = validated_data.get('youtubeUrl', instance.youtubeUrl)
         instance.mapUrl = validated_data.get('mapUrl', instance.mapUrl)
         instance.pdfManual = validated_data.get('pdfManual', instance.pdfManual)
-        instance.description = validated_data.get('description', instance.description)
         instance.equipment = validated_data.get('equipment', instance.equipment)
         instance.save()
 
-
-        # Handle nested updates
+        # Handle nested updates without deletion
         faqs_data = validated_data.pop('faqs', [])
         highlights_data = validated_data.pop('highlights', [])
         gallery_data = validated_data.pop('gallery', [])
@@ -245,6 +242,143 @@ class UpdateHolidaysPackagesSerializer(serializers.ModelSerializer):
                 HolidaysPackagesDates.objects.create(package=instance, **date_data)
 
         return instance
+
+
+
+# class UpdateHolidaysPackagesSerializer(serializers.ModelSerializer):
+#     faqs = HolidaysPackagesFaqSerializer(many=True, required=False)
+#     highlights = HolidaysPackagesHighlightsSerializer(many=True, required=False)
+#     gallery = HolidaysPackagesGallerySerializer(many=True, required=False)
+#     videos = HolidaysPackagesVideosSerializer(many=True, required=False)
+#     inclusions = HolidaysPackagesInclusionSerializer(many=True, required=False)
+#     exclusions = HolidaysPackagesExclusionSerializer(many=True, required=False)
+#     notices = HolidaysPackagesNoticeSerializer(many=True, required=False)
+#     dates = HolidaysPackagesDatesSerializer(many=True, required=False)
+
+#     class Meta:
+#         model = HolidaysPackages
+#         fields = [
+#             'id','country','city', 'packagetype', 'category', 'ordering', 'name', 'slug', 'description',
+#             'image', 'price', 'destination', 'duration', 'start_point', 'end_point',
+#             'group_size', 'season', 'meals', 'select_package_type', 'accommodations',
+#             'activity_duration', 'max_altitude', 'youtubeUrl', 'mapUrl', 'pdfManual',
+#             'faqs', 'highlights', 'gallery', 'videos', 'inclusions', 'exclusions', 'notices', 'dates'
+#         ]
+
+#     def update(self, instance, validated_data):
+#         # Update the HolidaysPackages fields
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.country = validated_data.get('country', instance.country)
+#         instance.city = validated_data.get('city', instance.city)
+#         instance.image = validated_data.get('image', instance.image)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.price = validated_data.get('price', instance.price)
+#         instance.destination = validated_data.get('destination', instance.destination)
+#         instance.duration = validated_data.get('duration', instance.duration)
+#         instance.start_point = validated_data.get('start_point', instance.start_point)
+#         instance.end_point = validated_data.get('end_point', instance.end_point)
+#         instance.group_size = validated_data.get('group_size', instance.group_size)
+#         instance.season = validated_data.get('season', instance.season)
+#         instance.meals = validated_data.get('meals', instance.meals)
+#         instance.select_package_type = validated_data.get('select_package_type', instance.select_package_type)
+#         instance.accommodations = validated_data.get('accommodations', instance.accommodations)
+#         instance.activity_duration = validated_data.get('activity_duration', instance.activity_duration)
+#         instance.max_altitude = validated_data.get('max_altitude', instance.max_altitude)
+#         instance.youtubeUrl = validated_data.get('youtubeUrl', instance.youtubeUrl)
+#         instance.mapUrl = validated_data.get('mapUrl', instance.mapUrl)
+#         instance.pdfManual = validated_data.get('pdfManual', instance.pdfManual)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.equipment = validated_data.get('equipment', instance.equipment)
+#         instance.save()
+
+
+#         # Handle nested updates
+#         faqs_data = validated_data.pop('faqs', [])
+#         highlights_data = validated_data.pop('highlights', [])
+#         gallery_data = validated_data.pop('gallery', [])
+#         videos_data = validated_data.pop('videos', [])
+#         inclusions_data = validated_data.pop('inclusions', [])
+#         exclusions_data = validated_data.pop('exclusions', [])
+#         notices_data = validated_data.pop('notices', [])
+#         dates_data = validated_data.pop('dates', [])
+
+#         # Update or create related objects
+#         for faq_data in faqs_data:
+#             faq_id = faq_data.get('id')
+#             if faq_id:
+#                 faq = HolidaysPackagesFaq.objects.get(id=faq_id, package=instance)
+#                 faq.question = faq_data.get('question', faq.question)
+#                 faq.answer = faq_data.get('answer', faq.answer)
+#                 faq.save()
+#             else:
+#                 HolidaysPackagesFaq.objects.create(package=instance, **faq_data)
+
+#         for highlight_data in highlights_data:
+#             highlight_id = highlight_data.get('id')
+#             if highlight_id:
+#                 highlight = HolidaysPackagesHighlights.objects.get(id=highlight_id, package=instance)
+#                 highlight.detail = highlight_data.get('detail', highlight.detail)
+#                 highlight.save()
+#             else:
+#                 HolidaysPackagesHighlights.objects.create(package=instance, **highlight_data)
+
+#         for gallery_data_item in gallery_data:
+#             gallery_id = gallery_data_item.get('id')
+#             if gallery_id:
+#                 gallery = HolidaysPackagesGallery.objects.get(id=gallery_id, package=instance)
+#                 gallery.image = gallery_data_item.get('image', gallery.image)
+#                 gallery.save()
+#             else:
+#                 HolidaysPackagesGallery.objects.create(package=instance, **gallery_data_item)
+
+#         for video_data in videos_data:
+#             video_id = video_data.get('id')
+#             if video_id:
+#                 video = HolidaysPackagesVideos.objects.get(id=video_id, package=instance)
+#                 video.video = video_data.get('video', video.video)
+#                 video.save()
+#             else:
+#                 HolidaysPackagesVideos.objects.create(package=instance, **video_data)
+
+#         for inclusion_data in inclusions_data:
+#             inclusion_id = inclusion_data.get('id')
+#             if inclusion_id:
+#                 inclusion = HolidaysPackagesInclusion.objects.get(id=inclusion_id, package=instance)
+#                 inclusion.detail = inclusion_data.get('detail', inclusion.detail)
+#                 inclusion.save()
+#             else:
+#                 HolidaysPackagesInclusion.objects.create(package=instance, **inclusion_data)
+
+#         for exclusion_data in exclusions_data:
+#             exclusion_id = exclusion_data.get('id')
+#             if exclusion_id:
+#                 exclusion = HolidaysPackagesExclusion.objects.get(id=exclusion_id, package=instance)
+#                 exclusion.detail = exclusion_data.get('detail', exclusion.detail)
+#                 exclusion.save()
+#             else:
+#                 HolidaysPackagesExclusion.objects.create(package=instance, **exclusion_data)
+
+#         for notice_data in notices_data:
+#             notice_id = notice_data.get('id')
+#             if notice_id:
+#                 notice = HolidaysPackagesNotice.objects.get(id=notice_id, package=instance)
+#                 notice.title = notice_data.get('title', notice.title)
+#                 notice.description = notice_data.get('description', notice.description)
+#                 notice.save()
+#             else:
+#                 HolidaysPackagesNotice.objects.create(package=instance, **notice_data)
+
+#         for date_data in dates_data:
+#             date_id = date_data.get('id')
+#             if date_id:
+#                 date = HolidaysPackagesDates.objects.get(id=date_id, package=instance)
+#                 date.start_date = date_data.get('start_date', date.start_date)
+#                 date.end_date = date_data.get('end_date', date.end_date)
+#                 date.save()
+#             else:
+#                 HolidaysPackagesDates.objects.create(package=instance, **date_data)
+
+#         return instance
     
 
 
@@ -406,6 +540,134 @@ class HolidaysPackagesItinerarySerializer(serializers.ModelSerializer):
         return itinerary
     
 
+# class UpdateHolidaysPackagesItinerarySerializer(serializers.ModelSerializer):
+#     accommodation = PackagesAccommodationSerializer(many=True, required=False)
+#     meals = PackagesMealsSerializer(many=True, required=False)
+#     activities = PackagesActivitiesSerializer(many=True, required=False)
+
+#     class Meta:
+#         model = HolidaysPackagesItinerary
+#         fields = ['id', 'day', 'title', 'description', 'image', 'accommodation', 'meals', 'activities']
+
+
+#     def update(self, instance, validated_data):
+#         accommodations_data = validated_data.pop('accommodation', [])
+#         meals_data = validated_data.pop('meals', [])
+#         activities_data = validated_data.pop('activities', [])
+        
+#         # instance.ordering = validated_data.get('ordering', instance.ordering)
+#         instance.day = validated_data.get('day', instance.day)
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.image = validated_data.get('image', instance.image)
+#         instance.save()
+
+#         # Update or create related objects
+#         for accommodation_data in accommodations_data:
+#             accommodation_id = accommodation_data.get('id')
+#             if accommodation_id:
+#                 accommodation = PackagesAccommodation.objects.get(id=accommodation_id, itinerary=instance)
+#                 accommodation.title = accommodation_data.get('title', accommodation.title)
+#                 accommodation.save()
+#             else:
+#                 PackagesAccommodation.objects.create(itinerary=instance, **accommodation_data)
+
+#         for meal_data in meals_data:
+#             meal_id = meal_data.get('id')
+#             if meal_id:
+#                 meal = PackagesMeals.objects.get(id=meal_id, itinerary=instance)
+#                 meal.title = meal_data.get('title', meal.title)
+#                 meal.save()
+#             else:
+#                 PackagesMeals.objects.create(itinerary=instance, **meal_data)
+
+#         for activity_data in activities_data:
+#             activity_id = activity_data.get('id')
+#             if activity_id:
+#                 activity = PackagesActivities.objects.get(id=activity_id, itinerary=instance)
+#                 activity.title = activity_data.get('title', activity.title)
+#                 activity.save()
+#             else:
+#                 PackagesActivities.objects.create(itinerary=instance, **activity_data)
+
+#         return instance
+
+
+# class UpdateHolidaysPackagesItinerarySerializer(serializers.ModelSerializer):
+#     accommodation = PackagesAccommodationSerializer(many=True, required=False)
+#     meals = PackagesMealsSerializer(many=True, required=False)
+#     activities = PackagesActivitiesSerializer(many=True, required=False)
+
+#     class Meta:
+#         model = HolidaysPackagesItinerary
+#         fields = ['id', 'day', 'title', 'description', 'image', 'accommodation', 'meals', 'activities']
+
+#     def update(self, instance, validated_data):
+#         accommodations_data = validated_data.pop('accommodation', [])
+#         meals_data = validated_data.pop('meals', [])
+#         activities_data = validated_data.pop('activities', [])
+
+#         instance.day = validated_data.get('day', instance.day)
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.image = validated_data.get('image', instance.image)
+#         instance.save()
+
+#         # Handle accommodation update or creation
+#         current_accommodations = {accommodation.id: accommodation for accommodation in instance.accommodation.all()}
+#         updated_accommodations = []
+
+#         for accommodation_data in accommodations_data:
+#             accommodation_id = accommodation_data.get('id')
+#             if accommodation_id and accommodation_id in current_accommodations:
+#                 accommodation = current_accommodations.pop(accommodation_id)
+#                 accommodation.title = accommodation_data.get('title', accommodation.title)
+#                 accommodation.save()
+#                 updated_accommodations.append(accommodation.id)
+#             else:
+#                 new_accommodation = PackagesAccommodation.objects.create(itinerary=instance, **accommodation_data)
+#                 updated_accommodations.append(new_accommodation.id)
+
+#         # Delete accommodations that were not updated
+#         PackagesAccommodation.objects.filter(itinerary=instance).exclude(id__in=updated_accommodations).delete()
+
+#         # Handle meals update or creation
+#         current_meals = {meal.id: meal for meal in instance.meals.all()}
+#         updated_meals = []
+
+#         for meal_data in meals_data:
+#             meal_id = meal_data.get('id')
+#             if meal_id and meal_id in current_meals:
+#                 meal = current_meals.pop(meal_id)
+#                 meal.title = meal_data.get('title', meal.title)
+#                 meal.save()
+#                 updated_meals.append(meal.id)
+#             else:
+#                 new_meal = PackagesMeals.objects.create(itinerary=instance, **meal_data)
+#                 updated_meals.append(new_meal.id)
+
+#         # Delete meals that were not updated
+#         PackagesMeals.objects.filter(itinerary=instance).exclude(id__in=updated_meals).delete()
+
+#         # Handle activities update or creation
+#         current_activities = {activity.id: activity for activity in instance.activities.all()}
+#         updated_activities = []
+
+#         for activity_data in activities_data:
+#             activity_id = activity_data.get('id')
+#             if activity_id and activity_id in current_activities:
+#                 activity = current_activities.pop(activity_id)
+#                 activity.title = activity_data.get('title', activity.title)
+#                 activity.save()
+#                 updated_activities.append(activity.id)
+#             else:
+#                 new_activity = PackagesActivities.objects.create(itinerary=instance, **activity_data)
+#                 updated_activities.append(new_activity.id)
+
+#         # Delete activities that were not updated
+#         PackagesActivities.objects.filter(itinerary=instance).exclude(id__in=updated_activities).delete()
+
+#         return instance
 class UpdateHolidaysPackagesItinerarySerializer(serializers.ModelSerializer):
     accommodation = PackagesAccommodationSerializer(many=True, required=False)
     meals = PackagesMealsSerializer(many=True, required=False)
@@ -415,45 +677,51 @@ class UpdateHolidaysPackagesItinerarySerializer(serializers.ModelSerializer):
         model = HolidaysPackagesItinerary
         fields = ['id', 'day', 'title', 'description', 'image', 'accommodation', 'meals', 'activities']
 
-
     def update(self, instance, validated_data):
         accommodations_data = validated_data.pop('accommodation', [])
         meals_data = validated_data.pop('meals', [])
         activities_data = validated_data.pop('activities', [])
-        
-        # instance.ordering = validated_data.get('ordering', instance.ordering)
+
         instance.day = validated_data.get('day', instance.day)
         instance.title = validated_data.get('title', instance.title)
         instance.description = validated_data.get('description', instance.description)
         instance.image = validated_data.get('image', instance.image)
         instance.save()
 
-        # Update or create related objects
+        # Handle accommodation update or creation
         for accommodation_data in accommodations_data:
             accommodation_id = accommodation_data.get('id')
             if accommodation_id:
+                # Update existing accommodation
                 accommodation = PackagesAccommodation.objects.get(id=accommodation_id, itinerary=instance)
                 accommodation.title = accommodation_data.get('title', accommodation.title)
                 accommodation.save()
             else:
+                # Create new accommodation
                 PackagesAccommodation.objects.create(itinerary=instance, **accommodation_data)
 
+        # Handle meals update or creation
         for meal_data in meals_data:
             meal_id = meal_data.get('id')
             if meal_id:
+                # Update existing meal
                 meal = PackagesMeals.objects.get(id=meal_id, itinerary=instance)
                 meal.title = meal_data.get('title', meal.title)
                 meal.save()
             else:
+                # Create new meal
                 PackagesMeals.objects.create(itinerary=instance, **meal_data)
 
+        # Handle activities update or creation
         for activity_data in activities_data:
             activity_id = activity_data.get('id')
             if activity_id:
+                # Update existing activity
                 activity = PackagesActivities.objects.get(id=activity_id, itinerary=instance)
                 activity.title = activity_data.get('title', activity.title)
                 activity.save()
             else:
+                # Create new activity
                 PackagesActivities.objects.create(itinerary=instance, **activity_data)
 
         return instance
