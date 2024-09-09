64f1456f82cfd5916e3e0d7d07ad58cbd5f4bd83
rmsh5413 <71429461+rmsh5413@users.noreply.github.com>
2024-09-09 10:43:04 +0545

    created serializrs and urls in country

--- toursandtravel/country/models.py
+++ toursandtravel/country/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from basemodel.models import BaseModel
+from useraccounts.models import User
+from autoslug import AutoSlugField
+from ckeditor.fields import RichTextField
+from django.core.validators import MaxValueValidator, MinValueValidator
+from packagestype.models import HolidaysPackagesType, HolidaysPackagesCategory
+
+# Create your models here.
+
+INTERNATIONALORDOMESTIC = (
+    ('International', 'International'),
+    ('Domestic', 'Domestic')
+)
+
+
+
+
+class Countries(BaseModel):
+    CONTINENTS = (
+            ('Africa', 'Africa'),
+            ('Asia', 'Asia'),
+            ('Europe', 'Europe'),
+            ('North America', 'North America'),
+            ('South America', 'South America'),
+            ('Oceania', 'Oceania'),
+            ('Antarctica', 'Antarctica')
+        )
+    
+    select_package_type=models.CharField(max_length=255, choices=INTERNATIONALORDOMESTIC,default='International')
+    continent = models.CharField(max_length=255, choices=CONTINENTS)
+    name = models.CharField(max_length=255)
+    ordering = models.PositiveBigIntegerField()
+    slug = AutoSlugField(populate_from='name', unique=True, always_update=True)
+    image = models.ImageField(upload_to='countries', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['ordering']
+
+
+
+class Cities(BaseModel):
+    country = models.ForeignKey(Countries, on_delete=models.CASCADE, related_name='cities',null=  True, blank=True)
+    name = models.CharField(max_length=255)
+    ordering=models.PositiveBigIntegerField()
+    slug = AutoSlugField(populate_from='name', unique=True,always_update=True)
+    image = models.ImageField(upload_to='cities', blank=True, null=True)
+    
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['ordering']
--- toursandtravel/packages/models.py
+++ toursandtravel/packages/models.py
@@ -5,6 +5,7 @@ from autoslug import AutoSlugField
 from ckeditor.fields import RichTextField
 from django.core.validators import MaxValueValidator, MinValueValidator
 from packagestype.models import HolidaysPackagesType, HolidaysPackagesCategory
+from country.models import Countries, Cities
 
 # Create your models here.
 
@@ -39,46 +40,6 @@ ACCOMMODATION = (
 
 
 
-
-class Countries(BaseModel):
-    CONTINENTS = (
-            ('Africa', 'Africa'),
-            ('Asia', 'Asia'),
-            ('Europe', 'Europe'),
-            ('North America', 'North America'),
-            ('South America', 'South America'),
-            ('Oceania', 'Oceania'),
-            ('Antarctica', 'Antarctica')
-        )
-    
-    select_package_type=models.CharField(max_length=255, choices=INTERNATIONALORDOMESTIC,default='International')
-    continent = models.CharField(max_length=255, choices=CONTINENTS)
-    name = models.CharField(max_length=255)
-    ordering = models.PositiveBigIntegerField()
-    slug = AutoSlugField(populate_from='name', unique=True, always_update=True)
-    image = models.ImageField(upload_to='countries', blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-    
-    class Meta:
-        ordering = ['ordering']
-
-
-
-class Cities(BaseModel):
-    country = models.ForeignKey(Countries, on_delete=models.CASCADE, related_name='cities',null=  True, blank=True)
-    name = models.CharField(max_length=255)
-    ordering=models.PositiveBigIntegerField()
-    slug = AutoSlugField(populate_from='name', unique=True,always_update=True)
-    image = models.ImageField(upload_to='cities', blank=True, null=True)
-    
-
-    def __str__(self):
-        return self.name
-    
-    class Meta:
-        ordering = ['ordering']
 
 class HolidaysPackages(BaseModel):
     country=models.ForeignKey(Countries, on_delete=models.CASCADE, related_name='countrypackages',null=  True, blank=True)
